e6153887eaab530d46a82f295f9c9a759945cc2c
Bai Jie <baijie1991@gmail.com>
2014-01-27 22:19:44 +0800

    add userList's cell formatter

--- src/org/simpleim/client/ChatController.java
+++ src/org/simpleim/client/ChatController.java
@@ -3,8 +3,10 @@ package org.simpleim.client;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.util.Callback;
 
 import org.simpleim.client.model.container.Account;
 import org.simpleim.client.model.netty.ChatClientHandler;
@@ -30,5 +32,24 @@ public class ChatController extends Controller {
 		mUserList = userList;
 		this.userList.setItems(mUserList);
 		return this;
+	}
+
+	@FXML
+	private void initialize() {
+		userList.setCellFactory(new Callback<ListView<Account>, ListCell<Account>>() {
+			@Override
+			public ListCell<Account> call(ListView<Account> list) {
+				return new AccountFormatListCell();
+			}
+		});
+	}
+
+	private static class AccountFormatListCell extends ListCell<Account> {
+		@Override
+		protected void updateItem(Account item, boolean empty) {
+			super.updateItem(item, empty);
+			setText(item == null ? "" : item.getNickname() == null ? item.getId() : item.getNickname());
+			// TODO online?
+		}
 	}
 }
