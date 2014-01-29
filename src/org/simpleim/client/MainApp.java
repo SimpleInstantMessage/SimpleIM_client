947007fe079a307da122c1da0b79a011dff03050
Bai Jie <baijie1991@gmail.com>
2014-01-30 02:17:50 +0800

    add icon

--- src/org/simpleim/client/MainApp.java
+++ src/org/simpleim/client/MainApp.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -20,6 +21,7 @@ public class MainApp extends Application {
 	public void start(Stage primaryStage) {
 		mPrimaryStage = primaryStage;
 		mPrimaryStage.setTitle("SimpleIM");
+		mPrimaryStage.getIcons().add(new Image("file:resources/images/icon.png"));
 
 		showLoginView();
 	}
